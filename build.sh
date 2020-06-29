04526bf8ff545c0838a80af1baa9a8629cd04ca0
csiew <clarence.siew@gmail.com>
2020-06-29 23:36:29 +1000

    Replaced create-session script with build script

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cp src/biscuitwm.py /usr/bin/biscuitwm.py
+cp assets/biscuitwm-session /usr/bin/biscuitwm-session
+chmod a+x /usr/bin/biscuitwm-session
+cp assets/biscuitwm-session.desktop /usr/share/xsessions/biscuitwm-session.desktop
--- create-session.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cp tinywm-session /usr/bin/tinywm-session
-chmod a+x /usr/bin/tinywm-session
-cp tinywm-session.desktop /usr/share/xsessions/tinywm-session.desktop
