6ab566ea8cd964e0bacbae00d0a15d2ad1a4a92d
csiew <clarence.siew@gmail.com>
2021-09-18 16:04:30 +1000

    Refactor keyboard event handler to use event name for reference

--- src/key_combs.py
+++ src/key_combs.py
@@ -1,51 +1,14 @@
-import pynput
-
 session = {
-    "terminal": {
-        pynput.keyboard.Key.alt,
-        pynput.keyboard.KeyCode(char='x')
-    },
-    "window_cycle": {
-        pynput.keyboard.Key.alt,
-        pynput.keyboard.Key.tab
-    },
-    "launcher": {
-        pynput.keyboard.Key.alt,
-        pynput.keyboard.KeyCode(char=' ')
-    },
-    "exit": {
-        pynput.keyboard.Key.alt,
-        pynput.keyboard.Key.esc
-    },
-}
-
-window_manipulation = {
-    "close": {
-        pynput.keyboard.Key.alt,
-        pynput.keyboard.KeyCode(char='q')
-    },
-    "maximize": {
-        pynput.keyboard.Key.alt,
-        pynput.keyboard.KeyCode(char='=')
-    },
-    "move_center": {
-        pynput.keyboard.Key.alt,
-        pynput.keyboard.KeyCode(char='-')
-    },
-    "move_left": {
-        pynput.keyboard.Key.alt,
-        pynput.keyboard.KeyCode(char='[')
-    },
-    "move_right": {
-        pynput.keyboard.Key.alt,
-        pynput.keyboard.KeyCode(char=']')
-    },
-    "move_top": {
-        pynput.keyboard.Key.alt,
-        pynput.keyboard.KeyCode(char='\\')
-    },
-    "move_bottom": {
-        pynput.keyboard.Key.alt,
-        pynput.keyboard.KeyCode(char='/')
-    },
+    "terminal": {"Alt", "Shift", "x"},
+    "window_cycle": {"Alt", "Shift", "Tab"},
+    "launcher": {"Alt", "Shift", "space"},
+    "exit": {"Alt", "Shift", "Escape"},
+    "close": {"Alt", "Shift", "q"},
+    "maximize": {"Alt", "Shift", "="},
+    "move_center": {"Alt", "Shift", "-"},
+    "move_left": {"Alt", "Shift", "["},
+    "move_right": {"Alt", "Shift", "]"},
+    "move_top": {"Alt", "Shift", "\\"},
+    "move_bottom": {"Alt", "Shift", "/"},
+    "focus": {"Alt", "Shift", "F1"},
 }
